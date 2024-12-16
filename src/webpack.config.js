5e5a03d21089489a2ffa38c9935c0694d7b9c69e
moonchi-k <jiwoo648@gmail.com>
2024-12-16 13:18:03 +0900

    test

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,15 +1,15 @@
-const CopyPlugin = require("copy-webpack-plugin");
+// const CopyPlugin = require("copy-webpack-plugin");
 
-module.exports = {
-  ...(plugins = [
-    ...new CopyPlugin({ patterns: [{ from: "public/assets", to: "assets/" }] }),
-  ]),
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "public"),
-    },
-    compress: true,
-    host: "localhost",
-    port: 3000,
-  },
-};
+// module.exports = {
+//   ...(plugins = [
+//     ...new CopyPlugin({ patterns: [{ from: "public/assets", to: "assets/" }] }),
+//   ]),
+//   devServer: {
+//     static: {
+//       directory: path.join(__dirname, "public"),
+//     },
+//     compress: true,
+//     host: "localhost",
+//     port: 3000,
+//   },
+// };
